17b4edba50869b31d5f7f34872f0a14811c97238
thihuli <thiagocandidof@hotmail.com>
2020-12-11 00:42:39 -0300

    persisting data with redux

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import HomeView from './views/Home.view'
-import store from './store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persisetdStore } from './store';
 import { Provider } from 'react-redux';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <HomeView />
+      <PersistGate persistor={persisetdStore}>
+        <HomeView />
+
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,6 @@
 import  { createStore, combineReducers } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
 
 import calculatorReducer from './calculator/Calculator.reducer';
 import productsReducer from './Products/Products.reducer';
@@ -8,7 +10,13 @@ const rootReducer = combineReducers({
   products: productsReducer
 });
 
-const store = createStore(rootReducer);
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = createStore(persistedReducer);
+export const persisetdStore = persistStore(store);
 
 
-export default store;
